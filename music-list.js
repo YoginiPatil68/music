d3734854d68d2cd69d1f2bf2396bda92833d1042
YoginiPatil68 <113588447+YoginiPatil68@users.noreply.github.com>
2023-07-31 21:32:13 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Agar Tum Sath Ho",
+    artist: "Alka Yagnik & Arijit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Deva Shree Ganesha",
+    artist: "Ajay-Atul",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "On And On",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Pasoori",
+    artist: "Ali Sethi & Shea Gill",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Lagdi Lahor Diya",
+    artist: "Guru Randhwa & Tulasi Kumar",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+];
